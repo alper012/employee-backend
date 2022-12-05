cd76004a9d0a270842cbd80ae676c7267ff227ea
Alper <alper.ekici@georgebrown.ca>
2022-12-04 21:37:47 -0500

    mongodb configs

--- server.js
+++ server.js
@@ -37,7 +37,11 @@ require("./app/routes/user.routes")(app);
 require("./app/routes/employee.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log('Server is running on port ${PORT}.');
+});
 
 
 function initial() {
